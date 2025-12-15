55e2f4ddc8105a39d526e1a38958de227c41d492
Alekhya Kurmindla <alekhya.kurmindla@gmail.com>
2025-12-15 19:01:08 +0530

    Removed unused methods

--- soldier/auth/token.go
+++ soldier/auth/token.go
@@ -3,37 +3,8 @@ package auth
 import (
 	"context"
 	"log"
-	"sync"
 	"time"
 )
-
-var (
-	// Ctx        context.Context
-	cancel   context.CancelFunc
-	tokenMu  sync.RWMutex
-	tokenExp time.Time
-)
-
-// Call this once during startup
-func StartTokenManager() {
-	Ctx, cancel = context.WithCancel(context.Background())
-	go func() {
-		for {
-			time.Sleep(25 * time.Second) // refresh BEFORE expiry
-
-			if time.Until(tokenExp) <= 5*time.Second {
-				log.Println("Auth token expiring — rotating token")
-
-				if !GetAuthWithRetry() {
-					log.Println("--->Token refresh failed — retrying")
-					continue
-				}
-
-				log.Println("Auth token rotated successfully")
-			}
-		}
-	}()
-}
 
 // Rotate token for every 30 seconds
 func RotateToken(ctx context.Context) {
